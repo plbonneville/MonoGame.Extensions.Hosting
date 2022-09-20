82c1e25066eee0eadf5cff16eb6bf09c150d6497
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-09-20 00:05:48 -0400

    Init WindowsDX

---
+++ tests/MonoGame.Extensions.Hosting.WindowsDX.IntegrationTests/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MonoGame.Extensions.Hosting;
+using MonoGame.Extensions.Hosting.WindowsDX.IntegrationTests;
+
+var builder = GameApplication.CreateBuilder(args).UseGame<Game1>();
+builder.Services.AddSingleton<IDemo, Demo>();
+using var game = builder.Build();
+await game.RunAsync();
