6622134f0b6f78ff08f63c6f6ccae4d05640b924
Pier-Luc Bonneville <9940262+plbonneville@users.noreply.github.com>
2022-09-29 09:44:01 -0400

    Deleted the sample project from the tests

--- tests/MonoGame.Extensions.Hosting.WindowsDX.IntegrationTests/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using MonoGame.Extensions.Hosting;
-using MonoGame.Extensions.Hosting.WindowsDX.IntegrationTests;
-
-var builder = GameApplication.CreateBuilder(args).UseGame<Game1>();
-builder.Services.AddSingleton<IDemo, Demo>();
-using var game = builder.Build();
-await game.RunAsync();
